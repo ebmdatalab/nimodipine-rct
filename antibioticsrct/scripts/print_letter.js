2bb87c83a5b9062a21cfb4f746c2fa4a00fc908b
Seb Bacon <seb.bacon@gmail.com>
2018-05-01 12:22:30 +0100

    Zoom so that we always fit on one page

--- antibioticsrct/scripts/print_letter.js
+++ antibioticsrct/scripts/print_letter.js
@@ -13,6 +13,22 @@ page.open(address, function (status) {
     phantom.exit(1);
   } else {
     window.setTimeout(function () {
+      page.evaluate(function(s) {
+        // force it to fit on A4
+        // For DPI discussion, see https://github.com/ebmdatalab/antibiotics-rct/issues/34
+        var dpi = 72;
+        var body = document.body,
+            html = document.documentElement;
+
+        var heightInPix = Math.max( body.scrollHeight, body.offsetHeight,
+                                    html.clientHeight, html.scrollHeight, html.offsetHeight );
+        var heightInCm = heightInPix * (2.54 / dpi);
+        var maxHeight = 27;  // alow for margins
+        if (heightInCm > maxHeight) {
+          document.body.style.zoom = maxHeight / heightInCm;
+        }
+        return;
+      });
       page.render(output);
       phantom.exit();
     }, 200);
