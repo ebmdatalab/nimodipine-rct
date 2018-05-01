7cbb5da95d2d879c382e38dcec613eee30cd64f8
Seb Bacon <seb.bacon@gmail.com>
2018-05-01 15:10:28 +0100

    Use live OP database for data

--- antibioticsrct/deploy/run-antibioticsrct.sh
+++ antibioticsrct/deploy/run-antibioticsrct.sh
@@ -2,4 +2,4 @@
 
 cd /var/www/antibioticsrct/antibiotics-rct/antibioticsrct
 
-. /etc/profile.d/antibioticsrct_staging.sh && exec ../../venv/bin/gunicorn antibioticsrct.wsgi -c deploy/gunicorn-antibioticsrct.conf.py
+. /etc/profile.d/antibioticsrct_live.sh && exec ../../venv/bin/gunicorn antibioticsrct.wsgi -c deploy/gunicorn-antibioticsrct.conf.py
